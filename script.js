a4b80fcfa32b5bed53f5c089efc078653c829700
miszczil <smishchil@gmail.com>
2015-06-09 20:55:57 +0200

    First day finished

--- script.js
+++ script.js
@@ -25,19 +25,27 @@ var questionArea = $("#question"),
     pytania = $("#pytania"),
     nextButton = $("#next"),
     questionNumber = 0,
+    currentQuestion,
+    allChoices,
+    correctAnswer,
     userAnswer,
     score = 0;
 
-var currentQuestion = allQuestions[questionNumber].question;
-questionContent.html(currentQuestion);
+var loadQuestion = function() {
 
-var allChoices = allQuestions[questionNumber].choices;
-for (var i in allChoices) {
-	pytania.append("<input type='radio' name='answer' value='" + i + "'>" + allChoices[i] + "<br/>");
+	currentQuestion = allQuestions[questionNumber].question;
+	questionContent.html(currentQuestion);
+
+	allChoices = allQuestions[questionNumber].choices;
+	for (var i in allChoices) {
+		pytania.append("<input type='radio' name='answer' value='" + i + "'>" + allChoices[i] + "<br/>");
+	}
+
+	correctAnswer = allQuestions[questionNumber].correctAnswer;
 }
 
-var correctAnswer = allQuestions[questionNumber].correctAnswer;
-  
+loadQuestion();
+
 var checkAnswer = function(){
       if (userAnswer == correctAnswer) {
         return true;
@@ -50,6 +58,16 @@ nextButton.click(function(){
   if (checkAnswer() == true) {
     score +=1;
   }
+  questionNumber +=1;
 
+  pytania.empty();
+  loadQuestion();
 
 });
+
+
+/*
+1. po ostatnim pytaniu zamienić Next na Submit - po kliknięciu wyświetl wynik
+2. 
+
+*/
