d5b1192dc38bb4f75d3159ca98098055c29de01f
bezliku <emhaw@o2.pl>
2015-06-09 19:32:22 +0200

    Questions created

---
+++ script.js
@@ -0,0 +1,22 @@
+var allQuestions = [
+
+{
+  question: "Who is Prime Minister of the United Kingdom?", 
+  choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+  correctAnswer:0
+},
+
+{
+  question: "Jakiego koloru jest pies?", 
+  choices: ["różowy", "brązowy", "niebieski w kropki", "przezroczysty"], 
+  correctAnswer:1
+},
+
+{
+  question: "Ile jest gruszek na wierzbie?", 
+  choices: ["100", "3", "1", "0"], 
+  correctAnswer:3
+}
+
+];
+
