1a30aca1420a8f88d3f28be8eaba4b8f5e65f1a5
miszczil <smishchil@gmail.com>
2015-06-16 20:41:03 +0200

    Submit function

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ var allQuestions = [
   choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
   correctAnswer:0
 },
-
 {
   question: "Jakiego koloru jest pies?", 
   choices: ["różowy", "brązowy", "niebieski w kropki", "przezroczysty"], 
   correctAnswer:1
 },
-
 {
   question: "Ile jest gruszek na wierzbie?", 
   choices: ["100", "3", "1", "0"], 
@@ -22,8 +20,9 @@ var allQuestions = [
 
 var questionArea = $("#question"),
     questionContent = $("#question > h2"),
-    pytania = $("#pytania"),
+    currentChoices = $("#choices"),
     nextButton = $("#next"),
+    submitButton = null,
     questionNumber = 0,
     currentQuestion,
     allChoices,
@@ -38,7 +37,7 @@ var loadQuestion = function() {
 
 	allChoices = allQuestions[questionNumber].choices;
 	for (var i in allChoices) {
-		pytania.append("<input type='radio' name='answer' value='" + i + "'>" + allChoices[i] + "<br/>");
+		currentChoices.append("<input type='radio' name='answer' value='" + i + "'>" + allChoices[i] + "<br/>");
 	}
 
 	correctAnswer = allQuestions[questionNumber].correctAnswer;
@@ -50,7 +49,8 @@ var checkAnswer = function(){
       if (userAnswer == correctAnswer) {
         return true;
     }
- };
+  };
+
 
 nextButton.click(function(){
 
@@ -60,14 +60,25 @@ nextButton.click(function(){
   }
   questionNumber +=1;
 
-  pytania.empty();
+  if (questionNumber >= allQuestions.length) {
+    $("#question").empty();
+    $("#question").append("<input id='submit' type='button' value='Submit'></input>");
+    submitButton = $("#submit");
+    return 0;
+  }
+
+  currentChoices.empty();
   loadQuestion();
 
 });
 
 
-/*
-1. po ostatnim pytaniu zamienić Next na Submit - po kliknięciu wyświetl wynik
-2. 
+$("#submit").click(function () {
 
-*/
+  $("#question").empty();
+  $("#question").append("<h2>Score: " + score + "</h2>"); 
+  console.log(score);
+  console.log("Submituje"); 
+
+});
+
